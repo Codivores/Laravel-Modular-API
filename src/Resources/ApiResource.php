7661a29c7b73a0520902b62fb8051cf12b5d4c1b
agnonym <contact@agnonym.com>
2024-08-20 11:00:47 +0200

    * | Resource - Api: use HashId / override toId() to handle Hashids feature

--- src/Resources/ApiResource.php
+++ src/Resources/ApiResource.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace Codivores\LaravelModularApi\Resources;
 
+use Codivores\LaravelModularApi\Traits\Features\HashId;
+use Illuminate\Http\Request;
 use TiMacDonald\JsonApi\JsonApiResource;
 
 class ApiResource extends JsonApiResource
 {
+    use HashId;
+
+    public function toId(Request $request)
+    {
+        $id = parent::toId($request);
+
+        if (! $this->isHashIdsFeatureEnabled()) {
+            return $id;
+        }
+
+        return $this->encode($id);
+    }
 }
