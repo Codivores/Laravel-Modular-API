e1a9e80436a644de0f1fbf625579f558a7d650c2
agnonym <contact@agnonym.com>
2024-08-20 11:01:54 +0200

    * | Resource - Api: use HasLinks

--- src/Resources/ApiResource.php
+++ src/Resources/ApiResource.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Codivores\LaravelModularApi\Resources;
 
 use Codivores\LaravelModularApi\Traits\Features\HashId;
+use Codivores\LaravelModularApi\Traits\Resources\HasLinks;
 use Illuminate\Http\Request;
 use TiMacDonald\JsonApi\JsonApiResource;
 
 class ApiResource extends JsonApiResource
 {
     use HashId;
+    use HasLinks;
 
     public function toId(Request $request)
     {
