7cf662c8f18d00593c0d07fc09d497de6f4ad27a
agnonym <contact@agnonym.com>
2024-08-20 11:02:21 +0200

    * | Resource - Api: override toType()

--- src/Resources/ApiResource.php
+++ src/Resources/ApiResource.php
@@ -7,6 +7,7 @@ namespace Codivores\LaravelModularApi\Resources;
 use Codivores\LaravelModularApi\Traits\Features\HashId;
 use Codivores\LaravelModularApi\Traits\Resources\HasLinks;
 use Illuminate\Http\Request;
+use LaravelModularApi;
 use TiMacDonald\JsonApi\JsonApiResource;
 
 class ApiResource extends JsonApiResource
@@ -23,5 +24,13 @@ class ApiResource extends JsonApiResource
         }
 
         return $this->encode($id);
+    }
+
+    public function toType(Request $request): string
+    {
+        return config('modular-api.api.resource.custom_type_resolver')
+            ? parent::toType($request)
+            : LaravelModularApi::domainFromClass($this)
+            .LaravelModularApi::resourceFromClass($this);
     }
 }
