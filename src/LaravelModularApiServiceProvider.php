528d4a1e01007f26ca64ccb206557fc6c1402a3d
agnonym <contact@agnonym.com>
2024-08-20 10:33:25 +0200

    + | Provider - LaravelModularApi

---
+++ src/LaravelModularApiServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codivores\LaravelModularApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelModularApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Handle the booting of the service provider.
+     */
+    public function boot(): void
+    {
+    }
+}
