e70ff3cd3e225ef5a043f0c69b1c78b06a7542e4
agnonym <contact@agnonym.com>
2024-08-20 11:05:10 +0200

    + | Controller - Base: use ApiController

---
+++ src/Http/Controllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codivores\LaravelModularApi\Http\Controllers;
+
+use Codivores\LaravelModularApi\Traits\Http\ApiController;
+
+class BaseController
+{
+    use ApiController;
+}
