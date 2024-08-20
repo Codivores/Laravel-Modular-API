1fdad5e8b1efd574d794ef098dae9acb9783d22a
agnonym <contact@agnonym.com>
2024-08-20 10:43:23 +0200

    + | Trait - Services - Util

---
+++ src/Traits/Services/Util.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codivores\LaravelModularApi\Traits\Services;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+trait Util
+{
+    public function domainNameList(): array
+    {
+        return collect($this->domainPathList())
+            ->map(function ($path) {
+                return basename($path);
+            })
+            ->toArray();
+    }
+
+    public function domainPathList(): array
+    {
+        $servicesPath = app_path(
+            config('modular-api.services.root_path')
+        );
+
+        return File::isDirectory($servicesPath)
+            ? File::directories($servicesPath)
+            : [];
+    }
+
+    public function servicePathList(): array
+    {
+        $servicePathList = [];
+        foreach ($this->domainNameList() as $name) {
+            foreach ($this->servicePathListForDomain($name) as $servicePath) {
+                $servicePathList[] = $servicePath;
+            }
+        }
+
+        return $servicePathList;
+    }
+
+    public function servicePathListForDomain(string $domain): array
+    {
+        return File::directories(
+            app_path(
+                config('modular-api.services.root_path')
+                .DIRECTORY_SEPARATOR
+                .$domain
+            )
+        );
+    }
+
+    public function servicesClassPathRoot(): string
+    {
+        return 'App\\'
+            .config('modular-api.services.root_path')
+            .'\\';
+    }
+
+    public function domainFromClass(object|string $class): string
+    {
+        return Str::before(
+            Str::replaceStart(
+                $this->servicesClassPathRoot(),
+                '',
+                get_class($class)
+            ),
+            '\\'
+        );
+    }
+
+    public function resourceFromClass(object|string $class): string
+    {
+        $delimiter = '_';
+
+        $classBaseNameExploded = explode(
+            $delimiter,
+            Str::snake(
+                class_basename($class),
+                $delimiter
+            )
+        );
+
+        return Str::studly(
+            implode(
+                $delimiter,
+                Arr::take(
+                    $classBaseNameExploded,
+                    count($classBaseNameExploded) - 1
+                )
+            )
+        );
+    }
+
+    public function domainSlug(string $resource): string
+    {
+        return Str::kebab($resource);
+    }
+
+    public function resourceSlug(string $resource): string
+    {
+        return Str::plural(
+            Str::kebab($resource)
+        );
+    }
+}
