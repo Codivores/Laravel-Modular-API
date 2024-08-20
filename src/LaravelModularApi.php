13a7d9ba52ad2b8f6d65387301e9153637a9d30b
agnonym <contact@agnonym.com>
2024-08-20 10:41:26 +0200

    * | Facade - LaravelModularApi: add apiUrl(), apiUrlPrefix(), apiRoutePrefix() from config

--- src/LaravelModularApi.php
+++ src/LaravelModularApi.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace Codivores\LaravelModularApi;
 
+use Illuminate\Support\Str;
+
 class LaravelModularApi
 {
+    public function apiUrl(): string
+    {
+        return config('modular-api.api.routing.url', '');
+    }
+
+    public function apiUrlPrefix(): string
+    {
+        return config('modular-api.api.routing.url_prefix', '');
+    }
+
+    public function apiRoutePrefix(): string
+    {
+        return config('modular-api.api.routing.route_prefix')
+            ? Str::finish(config('modular-api.api.routing.route_prefix'), '.')
+            : '';
+    }
 }
