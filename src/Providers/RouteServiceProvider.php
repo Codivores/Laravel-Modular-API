a3fd9a9af72efbc2fa2dadf0a0047e25999e74a2
agnonym <contact@agnonym.com>
2024-11-13 19:18:03 +0100

    * | Provider - Route: use WebRoute trait / call loadWebRoutes() in boot()

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 namespace Codivores\LaravelModularApi\Providers;
 
 use Codivores\LaravelModularApi\Traits\Http\ApiRoute;
+use Codivores\LaravelModularApi\Traits\Http\WebRoute;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    use ApiRoute;
+    use ApiRoute, WebRoute;
 
     public function boot(): void
     {
         $this->registerApiMacros();
         $this->loadApiRoutes();
+
+        $this->loadWebRoutes();
     }
 }
