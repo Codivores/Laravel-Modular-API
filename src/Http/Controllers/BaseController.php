67a5407e88a632b7d0e51d97a4bfdb5200c948ff
agnonym <contact@agnonym.com>
2024-12-17 12:49:07 +0100

    * | Controller - Base: use ApiResponse

--- src/Http/Controllers/BaseController.php
+++ src/Http/Controllers/BaseController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Codivores\LaravelModularApi\Http\Controllers;
 
 use Codivores\LaravelModularApi\Traits\Http\ApiController;
+use Codivores\LaravelModularApi\Traits\Http\ApiResponse;
 
 class BaseController
 {
     use ApiController;
+    use ApiResponse;
 }
